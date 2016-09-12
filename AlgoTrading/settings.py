1f5319a57246ed5e7df24bfafa1ff546ee3ac763
Anupreet Sehgal <Akshar@Akshar-Patels-MacBook-Pro.local>
2016-09-12 18:15:57 -0400

    Commit Before Adding Simple Moving Average

--- AlgoTrading/settings.py
+++ AlgoTrading/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'algo.apps.AlgoConfig',
+    'algo',
 ]
 
 MIDDLEWARE_CLASSES = [
--- algo/tasks.py
+++ algo/tasks.py
@@ -2,19 +2,22 @@ from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
 
-import algo.utils import runAlgo
+from algo.utils import runAlgo
+
 
 logger = get_task_logger(__name__)
 
 
-@periodic_task(
-    run_every=(crontab(minute='*/15')),
-    name="task_run_simple",
-    ignore_result=True
-)
-def SimpleMovingAlgo():
-    """
-    Runs the Simple Moving Algo
-    """
+#@periodic_task(
+#    run_every=(crontab(minute='*/1')),
+#    name="SimpleMovingAlgo",
+#    ignore_result=True
+#)
+
+
+@periodic_task(run_every=(crontab(minute='*/1')), name="some_task", ignore_result=True)
+def some_task():
+#   
     runAlgo()
+    
     logger.info("Running the Simple Moving Average Algo")
